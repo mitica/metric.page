e7ed60a0be55c102ca0df0241d63c67906f2c97a
Dumitru C. <canteadumitru@gmail.com>
2026-05-09 21:52:50 +0300

    tr.json

--- src/lib/locales/languages.ts
+++ src/lib/locales/languages.ts
@@ -19,7 +19,7 @@ export const languages: Language[] = [
   { code: "sv", name: "Swedish", nativeName: "Svenska", dir: "ltr" },
   { code: "cs", name: "Czech", nativeName: "Čeština", dir: "ltr" },
   { code: "uk", name: "Ukrainian", nativeName: "Українська", dir: "ltr" },
-  // { code: "tr", name: "Turkish", nativeName: "Türkçe", dir: "ltr" },
+  { code: "tr", name: "Turkish", nativeName: "Türkçe", dir: "ltr" },
   { code: "ru", name: "Russian", nativeName: "Русский", dir: "ltr" },
   // { code: "ja", name: "Japanese", nativeName: "日本語", dir: "ltr" },
   // { code: "ko", name: "Korean", nativeName: "한국어", dir: "ltr" },
